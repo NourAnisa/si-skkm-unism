2ad202fc30c26b36683acd28269bc27fe18d100c
Nor Anisa <48305341+NourAnisa@users.noreply.github.com>
2025-05-14 15:43:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
-from routers import auth
+from routers import prestasi
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
 templates = Jinja2Templates(directory="templates")
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
-app.include_router(auth.router, prefix="/auth")
+app.include_router(prestasi.router, prefix="/prestasi")
 
 @app.get("/")
 def home():
-    return {"message": "Login Sistem SKKM aktif"}
+    return {"message": "Form Prestasi SKKM siap"}
