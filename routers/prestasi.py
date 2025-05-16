c2133307f5d5028442acfc8a866b969028135543
Nor Anisa <48305341+NourAnisa@users.noreply.github.com>
2025-05-16 19:08:43 +0700

    Update prestasi.py

--- routers/prestasi.py
+++ routers/prestasi.py
@@ -1,50 +1,56 @@
+# routers/prestasi.py
 
-from fastapi import APIRouter, Request, Form
-from fastapi.templating import Jinja2Templates
+from fastapi import APIRouter, Form, Request, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from database import get_db
+from models import PrestasiSKKM
 from fastapi.responses import HTMLResponse
-from datetime import datetime
+from fastapi.templating import Jinja2Templates
+import uuid
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-@router.get("/input", response_class=HTMLResponse)
-def form_page(request: Request):
-    return templates.TemplateResponse("input_prestasi.html", {"request": request})
-
 @router.post("/submit", response_class=HTMLResponse)
-async def submit_form(
+async def submit_data(
     request: Request,
     nama: str = Form(...),
     nim: str = Form(...),
-    ta: str = Form(...),
+    tahun_ajaran: str = Form(...),
     semester: str = Form(...),
     kegiatan: str = Form(...),
     penyelenggara: str = Form(...),
     tingkat: str = Form(...),
     capaian: str = Form(...),
-    link_sertifikat: str = Form(...)
+    poin: int = Form(...),
+    file_path: str = Form(...),
+    db: AsyncSession = Depends(get_db)
 ):
-    poin = hitung_poin(tingkat, capaian)
-
+    new_data = PrestasiSKKM(
+        id=uuid.uuid4(),
+        nama=nama,
+        nim=nim,
+        tahun_ajaran=tahun_ajaran,
+        semester=semester,
+        kegiatan=kegiatan,
+        penyelenggara=penyelenggara,
+        tingkat=tingkat,
+        capaian=capaian,
+        poin_skkm=poin,
+        link_sertifikat=file_path,
+    )
+    db.add(new_data)
+    await db.commit()
     return templates.TemplateResponse("success.html", {
         "request": request,
         "nama": nama,
         "nim": nim,
-        "ta": ta,
+        "ta": tahun_ajaran,
         "semester": semester,
         "kegiatan": kegiatan,
         "penyelenggara": penyelenggara,
         "tingkat": tingkat,
         "capaian": capaian,
         "poin": poin,
-        "file_path": link_sertifikat
+        "file_path": file_path
     })
-
-def hitung_poin(tingkat: str, capaian: str) -> int:
-    map_poin = {
-        "Juara I": {"Lokal": 23, "Nasional": 50, "Internasional": 65},
-        "Juara II": {"Lokal": 18, "Nasional": 40, "Internasional": 55},
-        "Juara III": {"Lokal": 13, "Nasional": 30, "Internasional": 45},
-        "Peserta": {"Lokal": 5, "Nasional": 10, "Internasional": 25}
-    }
-    return map_poin.get(capaian, {}).get(tingkat, 0)
