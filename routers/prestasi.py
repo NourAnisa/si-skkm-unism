ee6cacf7eedc490038ff9e2ef4daec4376cb909d
Nor Anisa <48305341+NourAnisa@users.noreply.github.com>
2025-05-16 19:28:14 +0700

    Update prestasi.py

--- routers/prestasi.py
+++ routers/prestasi.py
@@ -1,9 +1,8 @@
-# routers/prestasi.py
-
-from fastapi import APIRouter, Request, Form
+from fastapi import APIRouter, Request, Form, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import SQLAlchemyError
 from database import get_db
 from models import PrestasiSKKM
 from utils import hitung_poin_skkm
@@ -30,7 +29,6 @@ async def simpan_data(
     link_sertifikat: str = Form(...),
     db: AsyncSession = Depends(get_db)
 ):
-    # Hitung poin SKKM otomatis
     poin = hitung_poin_skkm(tingkat, capaian)
 
     data = PrestasiSKKM(
@@ -46,18 +44,26 @@ async def simpan_data(
         poin_skkm=poin,
         link_sertifikat=link_sertifikat
     )
-    db.add(data)
-    await db.commit()
-    return templates.TemplateResponse("success.html", {
-        "request": request,
-        "nama": nama,
-        "nim": nim,
-        "ta": tahun_ajaran,
-        "semester": semester,
-        "kegiatan": kegiatan,
-        "penyelenggara": penyelenggara,
-        "tingkat": tingkat,
-        "capaian": capaian,
-        "poin": poin,
-        "file_path": link_sertifikat
-    })
+
+    try:
+        db.add(data)
+        await db.commit()
+        return templates.TemplateResponse("success.html", {
+            "request": request,
+            "nama": nama,
+            "nim": nim,
+            "ta": tahun_ajaran,
+            "semester": semester,
+            "kegiatan": kegiatan,
+            "penyelenggara": penyelenggara,
+            "tingkat": tingkat,
+            "capaian": capaian,
+            "poin": poin,
+            "file_path": link_sertifikat
+        })
+    except SQLAlchemyError as e:
+        await db.rollback()
+        return templates.TemplateResponse("input_prestasi.html", {
+            "request": request,
+            "error": "Gagal menyimpan data. " + str(e)
+        })
