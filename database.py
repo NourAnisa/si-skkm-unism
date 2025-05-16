a21aee0b3e020bf5ec126e85541f8dd53ae020e3
Nor Anisa <48305341+NourAnisa@users.noreply.github.com>
2025-05-16 21:51:14 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,26 +1,24 @@
 # database.py
-
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-# Supabase PostgreSQL connection URL (from environment variable)
 DATABASE_URL = os.getenv("SUPABASE_POSTGRES_URL_NON_POOLING")
 
-# Create async engine
+# Ganti "postgres" menjadi "postgresql+asyncpg" di URL jika belum
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://")
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-# Session
 AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False
 )
 
-# Base model
 Base = declarative_base()
 
-# Dependency to get DB session
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
